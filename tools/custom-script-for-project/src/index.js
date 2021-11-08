78e8ade7af1b3e0062f591d4ea0e96db9b6ba791
Apollo Tang <tang.apollo@gmail.com>
2021-11-08 00:44:03 -0500

    figure out script context

--- tools/custom-script-for-project/src/index.js
+++ tools/custom-script-for-project/src/index.js
@@ -1,2 +1,17 @@
 console.log('xxxxx message from "custom-script-for-project"')
 
+// https://flaviocopes.com/node-get-current-folder/
+
+// context is this package
+console.log('xxxx __dirname: ', __dirname)
+const jsonInSource = require('./json-in-source.json')
+console.log('xxxx jsonInSource: ', jsonInSource)
+
+// context is the consumer that installed package
+console.log('xxxx process.cwd(): ', process.cwd())
+const pathToJsonInConsumer = process.cwd() + '/json-in-consumer.json'
+const jsonInConsumer = require(pathToJsonInConsumer)
+console.log('xxxx jsonInConsumer: ', jsonInConsumer)
+
+
+
